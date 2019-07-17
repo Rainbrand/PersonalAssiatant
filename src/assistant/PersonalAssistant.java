9967dfcb3053ee638af21e3203ee4cc2d9c075c3
Rainbrand <26818172+Rainbrand@users.noreply.github.com>
2019-07-17 14:53:16 +0500

    First commit

---
+++ src/assistant/PersonalAssistant.java
@@ -0,0 +1,8 @@
+package assistant;
+
+public class PersonalAssistant {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2019.");
+    }
+}
